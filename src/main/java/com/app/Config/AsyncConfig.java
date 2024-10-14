ebdada6b142c5f258ca3913f9fcfd10ac3d3e896
thienphuc <thienphuc484@gmail.com>
2024-10-14 10:32:27 +0700

    Add vitural thread to async

--- src/main/java/com/app/Config/AsyncConfig.java
+++ src/main/java/com/app/Config/AsyncConfig.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @Configuration
 @EnableAsync
@@ -15,14 +16,7 @@ public class AsyncConfig implements AsyncConfigurer {
 
     @Override
     public Executor getAsyncExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(10);
-        executor.setKeepAliveSeconds(60);
-        executor.setMaxPoolSize(10);
-        executor.setQueueCapacity(20);
-        executor.setThreadNamePrefix("Test Async");
-        executor.initialize();
-        return executor;
+        return Executors.newVirtualThreadPerTaskExecutor();
     }
 
     @Override
