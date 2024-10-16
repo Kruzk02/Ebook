0efe54398dd8c6b80886911ee6d8054dd63c8b6a
thienphuc <thienphuc484@gmail.com>
2024-10-16 11:18:06 +0700

    Update SecurityConfig class

--- src/main/java/com/app/Config/SecurityConfig.java
+++ src/main/java/com/app/Config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.app.Config;
 
+import com.app.Exceptions.CustomAccessDeniedHandler;
 import com.app.Service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,12 +23,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.session.SessionRepository;
 import org.springframework.session.data.redis.RedisSessionRepository;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired private CustomAccessDeniedHandler accessDeniedHandler;
 
     @Primary
     public SessionRepository sessionRepository(RedisTemplate<String,Object> redisTemplate) {
@@ -60,7 +66,7 @@ public class SecurityConfig {
     @Bean
     public RoleHierarchy roleHierarchy(){
         RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
-        String hierarchy = "ROLE_ADMIN > ROLE_STAFF \n ROLE_STAFF > ROLE_USER";
+        String hierarchy = "ROLE_ADMIN > ROLE_USER";
         roleHierarchy.setHierarchy(hierarchy);
         return roleHierarchy;
     }
@@ -75,18 +81,29 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((authorize) ->
-                        authorize.requestMatchers(HttpMethod.GET,"/api/**").permitAll()
-                                .requestMatchers("/api/**").permitAll()
-                                .anyRequest().authenticated()
-                )
-                .sessionManagement(session ->
-                        session
-                                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-                                .maximumSessions(1).expiredUrl("/login?invalid-session=true")
-                                .maxSessionsPreventsLogin(true)
-                );
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests((authorize) ->
+                authorize.requestMatchers(
+                    HttpMethod.GET,"/api/**").permitAll()
+                    .requestMatchers("/api/user/info").authenticated()
+                    .requestMatchers("/api/auth/**").permitAll()
+                    .requestMatchers(HttpMethod.POST,"/api/author/create").hasAnyRole("ADMIN")
+                    .requestMatchers(HttpMethod.DELETE,"/api/author/delete/**").hasRole("ADMIN")
+                    .requestMatchers(HttpMethod.POST,"api/comment/create").authenticated()
+                    .requestMatchers(HttpMethod.DELETE,"api/comment/delete/**").authenticated()
+                    .requestMatchers(HttpMethod.POST,"/api/ebook/upload").hasRole("ADMIN")
+                    .requestMatchers(HttpMethod.POST,"/api/ebook/download/**").permitAll()
+                    .requestMatchers(HttpMethod.DELETE,"/api/ebook/delete/**").authenticated()
+                    .anyRequest().authenticated()
+            )
+            .exceptionHandling(e -> e.accessDeniedHandler(accessDeniedHandler)
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+            .sessionManagement(session ->
+                session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                        .maximumSessions(1)
+                        .expiredUrl("/login?invalid-session=true")
+            );
         return http.build();
     }
 
