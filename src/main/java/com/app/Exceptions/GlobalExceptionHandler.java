f13c83c8f8dd27712a82cc2faafa4ca14274b535
thienphuc <thienphuc484@gmail.com>
2024-10-23 10:00:25 +0700

    Update GlobalExceptionHandler for handing FIleTypeNotPDFException and FileSizeLargeException

--- src/main/java/com/app/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/app/Exceptions/GlobalExceptionHandler.java
@@ -1,15 +1,11 @@
 package com.app.Exceptions;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.io.IOException;
 
 @ControllerAdvice
 @Log4j2
@@ -17,26 +13,38 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 
     @ExceptionHandler(AuthorNotFoundException.class)
-    public ResponseEntity<?> handleAuthorNotFoundException(AuthorNotFoundException ex) {
+    public ResponseEntity<ErrorResponse> handleAuthorNotFoundException(AuthorNotFoundException ex) {
         ErrorResponse response = new ErrorResponse("Author Not Found");
-        return new ResponseEntity<>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(EBookNotFoundException.class)
-    public ResponseEntity<?> handleEBookNotFoundException(EBookNotFoundException ex) {
+    public ResponseEntity<ErrorResponse> handleEBookNotFoundException(EBookNotFoundException ex) {
         ErrorResponse response = new ErrorResponse("EBook Not Found");
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<?> handleUserNotFoundException(UserNotFoundException ex) {
+    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException ex) {
         ErrorResponse response = new ErrorResponse("User Not Found");
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(CommentNotFoundException.class)
-    public ResponseEntity<?> handleCommentNotFoundException(CommentNotFoundException ex){
+    public ResponseEntity<ErrorResponse> handleCommentNotFoundException(CommentNotFoundException ex){
         ErrorResponse response = new ErrorResponse("Comment Not Found");
         return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(FileTypeNotPDFException.class)
+    public ResponseEntity<ErrorResponse> handleFileTypeNotPDFException(FileTypeNotPDFException ex){
+        ErrorResponse response = new ErrorResponse("File type is not pdf");
+        return new ResponseEntity<>(response,HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+    }
+
+    @ExceptionHandler(FileSizeLargeException.class)
+    public ResponseEntity<ErrorResponse> handleFileSizeLargeException(FileSizeLargeException ex){
+        ErrorResponse response = new ErrorResponse("File is large than 10mb");
+        return new ResponseEntity<>(response,HttpStatus.PAYLOAD_TOO_LARGE);
     }
 }
